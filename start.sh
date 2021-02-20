1775efe8428141d2086dec1657d83074f1ed9a6f
onb6 <blierolivia@gmail.com>
2021-02-19 22:59:25 -0500

    added start script, deploy script, service file, and nginx config file

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export MIX_ENV=prod
+export PORT=6899
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/bulls/bin/bulls stop || true
+
+echo "Starting app..."
+
+_build/prod/rel/bulls/bin/bulls start
